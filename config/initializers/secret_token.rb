efea17983a0832847cd598fcf72d91390a99d050
Dorian <pr.dorian@gmail.com>
2011-03-31 22:47:32 +0200

    Génération de la structure avec une base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteEtu::Application.config.secret_token = '2619a75160149fec05d97a64f9c10eeea829d8b4bbddbcac3cf6b5568eebc81b730c19938faaee0c0c82511ca8fa5753296af49c5707ff34339ca7ad655afb93'
