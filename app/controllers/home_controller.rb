92360e33ec21cac90e9ca3f52f6e6c567766be1a
Dorian <pr.dorian@gmail.com>
2011-04-01 00:31:05 +0200

    Modifications légéres de la page d'accueil et de sa route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
+
   def index
+    # On mettra ici ce qu'il faut charger pour ensuite l'afficher sur la
+    # page d'accueil (news, widgets, …)
   end
 
 end
