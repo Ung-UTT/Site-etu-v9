3e2329bb2faaeee06a2da78f0862be71dee0882b
Dorian <pr.dorian@gmail.com>
2011-05-03 01:08:46 +0200

    Mauvaise appli Twitter (oui, je pollue le dépôt)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter,  'K5Owb3RGHlCpSY2epOQOhg', 'MbPBKOuK85ej0i5aP4Dt8CWFlAyWSntNheBD3NghDxk'
+  provider :twitter,  'kic7XPhWl6Sm2YInckig', 'm0sfx36vwWMcJuBi8MVtjMyZDUcMwFu3Z6IcApAg'
   provider :CAS,  :cas_server => 'https://cas.utt.fr/cas/'
   provider :open_id, OpenID::Store::Filesystem.new('/tmp'), {:name => "google", :domain => "https://www.google.com/accounts/o8/id" }
 end
