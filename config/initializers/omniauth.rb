09681276ca53bac0b667a660b92fa48b0bf7450e
Dorian <pr.dorian@gmail.com>
2011-05-03 01:26:25 +0200

    Le CAS, OpenID et Twitter fonctionnent !

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter,  'kic7XPhWl6Sm2YInckig', 'm0sfx36vwWMcJuBi8MVtjMyZDUcMwFu3Z6IcApAg'
   provider :CAS,  :cas_server => 'https://cas.utt.fr/cas/'
-  provider :open_id, OpenID::Store::Filesystem.new('/tmp'), {:name => "google", :domain => "https://www.google.com/accounts/o8/id" }
+  provider :open_id, OpenID::Store::Filesystem.new('/tmp')
 end
