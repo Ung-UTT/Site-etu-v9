b593bc8716955a39535a2a734e0132f8239b11bc
Dorian <pr.dorian@gmail.com>
2011-06-14 20:06:19 +0200

    (SEO URLs) Outch, un bug s'était glissé, c'est mieux là

--- config/initializers/to_param_seo.rb
+++ config/initializers/to_param_seo.rb
@@ -2,10 +2,10 @@ module ActiveRecord
   class Base
     def to_param
       param_string = attributes.keys.select {|k| ['name', 'title', 'login', 'content'].include?(k) }.first
-      param_string = send(param_string)
       if param_string.nil?
-        return id
+        super
       else
+        param_string = send(param_string)
         param_string = param_string.slice(0..80).gsub(/[^a-z0-9]+/i, '-').chomp('-')
         return "#{id}-#{param_string}"
       end
