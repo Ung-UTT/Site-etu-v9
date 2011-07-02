0e491eb0a3d0db5a91154118d7c79bc57dbc0c46
Dorian <pr.dorian@gmail.com>
2011-07-03 01:27:58 +0200

    Changements dans le TODO, et config des mails en production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,10 @@ SiteEtu::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => 'smtp.utt.fr',
+    :port                 => 25,
+    :authentication       => 'plain' }
 end
