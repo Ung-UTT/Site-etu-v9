9d8cdbdcf471a88e4abcb41d7b5e0036800ec6b4
Dorian <pr.dorian@gmail.com>
2011-10-26 00:39:36 +0200

    Kanami code refonctionne

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
-var keys = [38, 38, 40, 40, 37, 37, 39, 39, 66, 65];
+var keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 var progress = 0;
 
-$('*').keyup(function(event) {
+$('body').keyup(function(event) {
   if (event.keyCode == 27) {
     $('body').html('<div>Blue Screen Of Death</div>').css({'background-color': 'blue', 'background-image': 'url()'}).remove;
     $('div').css({'font-size': '8em', 'text-weight': 'bolder', 'text-align': 'center', 'margin-top': '150px'});
@@ -9,6 +9,8 @@ $('*').keyup(function(event) {
 
   if (event.keyCode == keys[progress]) {
     progress += 1;
+    console.log(progress);
+    console.log(event.keyCode);
     if (progress == keys.length) {
       $('a, p, li, h1, h2, h3, header').click(function () {
         $(this).fadeOut('slow');
