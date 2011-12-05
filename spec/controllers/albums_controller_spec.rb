f2c3b9d70d6433b2fbe2017b4f39ac9c7007b1ca
Dorian <pr.dorian@gmail.com>
2011-12-05 09:48:56 +0100

    Les fixtures seront necessaires avant de faire les tests fonctionnels

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -1,19 +1,3 @@
-describe AlbumsController, "#show" do
-  context "for a fictionnal album" do
-    before do
-      get :show, :id => 1
-    end
+require 'spec_helper'
 
-    it { should assign_to(:album) }
-    it { should respond_with(:success) }
-    it { should render_template(:show) }
-    it { should_not set_the_flash }
-  end
-end
-
-describe AlbumsController, "#index" do
-  it { should assign_to(:albums) }
-  it { should respond_with(:success) }
-  it { should render_template(:index) }
-  it { should_not set_the_flash }
-end
+#TODO: Faire les fixtures avant
