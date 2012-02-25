7ed9d333bc2721a58093b43aede02ebad1341177
Dorian <pr.dorian@gmail.com>
2012-02-25 02:16:08 +0100

    HTML/CSS : h1 du titre et du contenu avec des styles différents

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
 
   def title(page_title, options={})
     content_for(:title, page_title.to_s)
-    content_tag(:h1, page_title, options)
+    content_tag(:h1, page_title, options.merge(:class =>'title'))
   end
 
   def title_tag
