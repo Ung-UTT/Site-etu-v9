62110020533d1dbe358182c6633aa71018f8c746
Dorian <pr.dorian@gmail.com>
2012-03-09 17:23:51 +0100

    Intégration de chosen
    Permet de choisir parmit une longue liste (utilisateurs, UVs, événements, ...)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,26 +1,34 @@
 //= require jquery
 //= require rails
+//= require chosen
 
-var keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-var progress = 0;
+$(function () {
+  $('select.chosen').chosen();
 
-$('body').keyup(function(event) {
-  if (event.keyCode == 27) {
-    $('body').html('<div>Blue Screen Of Death</div>').css({'background-color': 'blue', 'background-image': 'url()'}).remove;
-    $('div').css({'font-size': '8em', 'text-weight': 'bolder', 'text-align': 'center', 'margin-top': '150px'});
-  }
+  // Code Konami (suppprime éléments quand on clique dessus
+  // et "Echap" : met un BSOD
+  var keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  var progress = 0;
 
-  if (event.keyCode == keys[progress]) {
-    progress += 1;
-    console.log(progress);
-    console.log(event.keyCode);
-    if (progress == keys.length) {
-      $('a, p, li, h1, h2, h3, header').click(function () {
-        $(this).fadeOut('slow');
-        return false;
-      });
+  $('body').keyup(function(event) {
+    if (event.keyCode == 27) {
+      $('body').html('Blue Screen Of Death').css({'background-color': 'blue',
+        'background-image': 'url()', 'font-size': '8em', 'text-weight': 'bolder',
+        'text-align': 'center', 'margin-top': '150px', 'box-shadow': 'none'});
     }
-  } else {
-    progress = 0;
-  }
+
+    if (event.keyCode == keys[progress]) {
+      progress += 1;
+      console.log(progress);
+      console.log(event.keyCode);
+      if (progress == keys.length) {
+        $('a, p, li, h1, h2, h3, header').click(function () {
+          $(this).fadeOut('slow');
+          return false;
+        });
+      }
+    } else {
+      progress = 0;
+    }
+  });
 });
