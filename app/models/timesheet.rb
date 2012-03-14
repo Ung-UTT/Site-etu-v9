b1235bc6b84696f47138874050ecb3767b582f45
Dorian <pr.dorian@gmail.com>
2012-03-14 01:43:07 +0100

    Agenda : Améliore encore un peu l'affichage

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -32,7 +32,7 @@ class Timesheet < ActiveRecord::Base
   # Traduit l'horaire en un hash exploitable par FullCalendar
   def to_fullcalendar
 
-    return { 'title' => "#{course.name} #{room} #{category}",
+    return { 'title' => "#{course.name}\n#{room}\n#{category}",
              'start_at' => start_at,
              'end_at' => end_at,
              'object' => self,
