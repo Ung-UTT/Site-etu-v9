c84b18f98feb14db179b2de5bd6c44393c2c99d2
Dorian <pr.dorian@gmail.com>
2012-03-19 02:02:37 +0100

    Sondage : pourcentage de votes par question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,8 +6,18 @@ class Answer < ActiveRecord::Base
   belongs_to :poll
   has_many :votes
 
-  # Est-ce que l'utilisateur a répondu à cette question
+  # Est-ce que l'utilisateur a répondu à cette réponse ?
   def voted_by?(user)
     votes.map(&:user_id).include?(user.id)
+  end
+
+  # Pourcentages de votes pour cette réponse
+  def percent
+    count_all = poll.votes.size.to_f
+    if count_all == 0
+      return 0
+    else
+      return (100 * votes.size / count_all).round.to_i
+    end
   end
 end
