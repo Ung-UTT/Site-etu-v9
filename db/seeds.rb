44563cd24c36e9ac566c1340894c0c0fd70cbe2b
Cédric FELIZARD <cedric@felizard.fr>
2012-04-10 01:08:33 +0200

    Crazy fingers...

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 role_admin = Role.create(name: 'admin')
 role_moderator = Role.create(name: 'moderator')
 
-admin = User.create(login: 'admin', password: 'changez-moi', email: 'admin@exmaple.com')
-moderator = User.create(login: 'moderator', password: 'changez-moi', email: 'moderator@exmaple.com')
+admin = User.create(login: 'admin', password: 'changez-moi', email: 'admin@example.com')
+moderator = User.create(login: 'moderator', password: 'changez-moi', email: 'moderator@example.com')
 
 admin.roles << role_admin
 admin.save
