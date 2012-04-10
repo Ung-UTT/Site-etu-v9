3e2253e2251bc6c01b2a9c151a67900f687f0542
Cédric FELIZARD <cedric@felizard.fr>
2012-04-10 14:59:52 +0200

    Most recent news on first page

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -5,7 +5,7 @@ class NewsController < ApplicationController
   # GET /news
   # GET /news.xml
   def index
-    @news = News.page(params[:page])
+    @news = News.order('updated_at DESC').page(params[:page])
 
     respond_to do |format|
       format.html # index.html.erb
