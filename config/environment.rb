36e724c296798d9da5348abe1811c1dfaf67a4c9
Cédric FELIZARD <cedric@felizard.fr>
2012-05-04 17:20:49 +0200

    Typo

--- config/environment.rb
+++ config/environment.rb
@@ -6,4 +6,4 @@ Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
 # Initialize the rails application
-SiteEtu::Application.initializn
+SiteEtu::Application.initialize!
