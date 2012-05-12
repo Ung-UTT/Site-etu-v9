0c8f9588f95ae0a1b3b812be0e495776085b884e
Dorian <pr.dorian@gmail.com>
2012-05-12 17:33:33 +0200

    Assos en HAML et vues mieux écrites

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # Set UTF-8 as default encoding
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
+Encoding.default_internal = Encoding.default_external = Encoding::UTF_8
 
 # Initialize the rails application
 SiteEtu::Application.initialize!
-
