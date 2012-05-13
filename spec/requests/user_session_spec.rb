310a3c2d6a9ed0ecb6b5d1df0b972ce35b1f6f4a
Cédric FELIZARD <cedric@felizard.fr>
2012-05-14 00:54:40 +0200

    Wording

--- spec/requests/user_session_spec.rb
+++ spec/requests/user_session_spec.rb
@@ -1,24 +1,25 @@
 require 'spec_helper'
 
-feature "Logging" do
+feature "Logging in/out" do
+  let(:logout_form) { '//form[@action="/users/sign_out"]' }
+
   background do
     @user = create :user
-    @logout_detect = '//form[@action="/users/sign_out"]'
   end
 
-  scenario "Login then logout" do
+  scenario "Logging in then logging out" do
     sign_in @user.login, @user.password
     current_path.should == '/'
-    logout = find(@logout_detect)
+    logout = find(logout_form)
 
     submit_form
     current_path.should == '/'
-    page.should_not have_xpath(@logout_detect)
+    page.should_not have_xpath(logout_form)
   end
 
   scenario "Trying to log in with incorrect credentials" do
     sign_in @user.login, @user.password * 2
     current_path.should == '/users/sign_in'
-    page.should_not have_xpath(@logout_detect)
+    page.should_not have_xpath(logout_form)
   end
 end
