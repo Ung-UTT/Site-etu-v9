ebc077cf57ea6674b9735a0209f84af58e632db5
Cédric FELIZARD <cedric@felizard.fr>
2012-05-26 00:23:29 +0200

    Don't use Redis for now

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-if Rails.env.production?
-  SiteEtu::Application.config.session_store :redis_store
-else
-  SiteEtu::Application.config.session_store :cookie_store, key: '_site_etu_session'
-end
+SiteEtu::Application.config.session_store :cookie_store, key: '_site_etu_session'
 
