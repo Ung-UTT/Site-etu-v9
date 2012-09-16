e70c619cbb3ffaa1fe1f252e63aa18dc4c87d1f6
Cédric Félizard <cedric@felizard.fr>
2012-09-16 21:52:27 +0200

    Use the new RSpec random order to reveal dependency issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -59,6 +59,12 @@ Spork.prefork do
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
 
+    # Run specs in random order to surface order dependencies. If you find an
+    # order dependency and want to debug it, you can fix the order by providing
+    # the seed, which is printed after each run.
+    #     --seed 1234
+    config.order = 'random'
+
     config.before(:each) do
       # reset PaperTrail so data from one test doesn't spill over another
       PaperTrail.controller_info = {}
